28a3ad33e962e680856548dbe85dd42411ea16ee
Lars-Magnus Skog <ralphtheninja@riseup.net>
2020-12-30 15:40:52 +0100

    day23

---
+++ 2020/day23/part1.js
@@ -0,0 +1,79 @@
+const { deepStrictEqual, notEqual } = require('assert')
+
+function pickUp (current, cups) {
+  const out = []
+  let count = 0
+  while (count < 3) {
+    const index = cups.indexOf(current)
+    notEqual(index, -1, 'current must exist')
+    if (index >=0 && index < cups.length - 1) {
+      out.push(cups.splice(index + 1, 1)[0])
+    } else if (index === cups.length - 1) {
+      out.push(cups.splice(0, 1)[0])
+    }
+    count++
+  }
+  return out
+}
+
+deepStrictEqual(pickUp(3, [3, 8, 9, 1, 2, 5, 4, 6, 7]), [8, 9, 1])
+deepStrictEqual(pickUp(1, [9, 2, 5, 4, 8, 1, 3, 6, 7]), [3, 6, 7])
+deepStrictEqual(pickUp(9, [7, 2, 5, 8, 4, 1, 9, 3, 6]), [3, 6, 7])
+deepStrictEqual(pickUp(2, [8, 3, 6, 7, 4, 1, 9, 2, 5]), [5, 8, 3])
+deepStrictEqual(pickUp(6, [7, 4, 1, 5, 8, 3, 9, 2, 6]), [7, 4, 1])
+
+function findDestination (current, cups) {
+  let destination = current - 1
+  while (destination >= 1) {
+    const index = cups.indexOf(destination)
+    if (index !== -1) {
+      return destination
+    }
+    destination--
+  }
+  return cups.reduce((tot, el) => Math.max(tot, el), 0)
+}
+
+deepStrictEqual(findDestination(3, [3, 2, 5, 4, 6, 7]), 2)
+deepStrictEqual(findDestination(2, [3, 2, 5, 4, 6, 7]), 7)
+deepStrictEqual(findDestination(1, [9, 2, 5, 4, 8, 1]), 9)
+deepStrictEqual(findDestination(9, [2, 5, 8, 4, 1, 9]), 8)
+deepStrictEqual(findDestination(2, [6, 7, 4, 1, 9, 2]), 1)
+deepStrictEqual(findDestination(6, [5, 8, 3, 9, 2, 6]), 5)
+deepStrictEqual(findDestination(5, [5, 8, 3, 9, 2, 6]), 3)
+
+function cupsResult (cups) {
+  const str = cups.join('')
+  const arr = str.split(1)
+  arr.reverse()
+  return arr.join('')
+}
+
+deepStrictEqual(cupsResult([5, 8, 3, 7, 4, 1, 9, 2, 6]), '92658374')
+deepStrictEqual(cupsResult([5, 8, 3, 7, 4, 9, 2, 6, 1]), '58374926')
+
+function moveCups (CUPS, MAX_MOVES) {
+  let moves = 0
+  let cups = CUPS.split('').map(Number)
+  let current = cups[0]
+
+  while (moves < MAX_MOVES) {
+    const pick = pickUp(current, cups)
+    const destination = findDestination(current, cups)
+    const destinationIndex = cups.indexOf(destination)
+    console.log('picked', pick)
+    pick.reverse().forEach(el => cups.splice(destinationIndex + 1, 0, el))
+    console.log('destination', destination, 'destination index', destinationIndex, 'cups after', cups)
+    const currentIndex = cups.indexOf(current)
+    let newCurrentIndex = currentIndex <= cups.length - 2 ? currentIndex + 1 : 0
+    current = cups[newCurrentIndex]
+    console.log('new current', current)
+    moves++
+  }
+
+  return cupsResult(cups)
+}
+
+//deepStrictEqual(moveCups('389125467', 10), '92658374')
+//deepStrictEqual(moveCups('389125467', 100), '67384529')
+console.log('result:', moveCups('215694783', 100))
---
+++ 2020/day23/part2.js
@@ -0,0 +1,95 @@
+const { deepStrictEqual } = require('assert')
+
+/**
+ * Clearly rewriting and searching arrays of 1M and doing this 10M times
+ * will take a lot of time. So lets build a circular list instead.
+ * We still might need to search for destination elements, but picking out
+ * and re-inserting the three elements should only be about changing
+ * 'pointer' values.
+ * Now each element is of the form { value, prev, next } where
+ * - value i the value
+ * - prev points to the previous element in the list
+ * - next points to the next element in the list
+ */
+function moveCups (CUPS, MAX_MOVES) {
+  const temp = CUPS.split('').map(Number)
+  for (let i = 10; i <= 1000000; ++i) {
+    temp.push(i)
+  }
+
+  // Index that takes a value and points to the corresponding
+  // element object (with .value, .prev and .next)
+  const index = {}
+
+  // Build circular list where current points to the first element
+  // all sub sequent elements points previous element to it so thereby
+  // setting it's own p and the previous n correctly.
+  // At the end of the list we hook the last element together with
+  // the first element (current)
+  let current = { value: temp[0], next: null, prev: null }
+  index[current.value] = current
+  let prev = current
+  let maxValue = 0
+
+  temp.forEach((value, i) => {
+    if (i > 0) {
+      const el = { value, next: null, prev }
+      index[value] = el
+      el.prev.next = el
+      prev = el
+      if (i === temp.length - 1) {
+        el.next = current
+        current.prev = el
+      }
+    }
+    maxValue = Math.max(maxValue, value)
+  })
+
+  function findDestination (current, picked) {
+    let destinationValue = current.value - 1
+    while (destinationValue >= 1 &&
+           picked.includes(destinationValue)) {
+      destinationValue--
+    }
+    if (destinationValue !== 0) {
+      return index[destinationValue]
+    } else {
+      destinationValue = maxValue
+      while (destinationValue >= 1 &&
+             picked.includes(destinationValue)) {
+        destinationValue--
+      }
+      return index[destinationValue]
+    }
+  }
+
+  let moves = 0
+  while (moves < MAX_MOVES) {
+    // Pick out three linked elements after current
+    const pickStart = current.next
+    const pickEnd = pickStart.next.next
+    const picked = [
+      pickStart.value,
+      pickStart.next.value,
+      pickEnd.value
+    ]
+    current.next = pickEnd.next
+    pickEnd.next.prev = current
+
+    // Find destination and link in picked elements after it
+    const destination = findDestination(current, picked)
+    pickEnd.next = destination.next
+    destination.next.prev = pickEnd
+    pickStart.prev = destination
+    destination.next = pickStart
+
+    current = current.next
+    moves++
+  }
+
+  const one = index[1]
+  return one.next.value * one.next.next.value
+}
+
+//deepStrictEqual(moveCups('389125467', 10000000), 934001 * 159792)
+console.log('result:', moveCups('215694783', 10000000))
