0701fc7bc150ffecebafda5c5b524a98d5625b43
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-12-06 21:43:55 +0100

    2022/04

---
+++ 2022/04/part1.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const assert = require('assert')
+
+function contains (one, two) {
+  return one[0] <= two[0] && one[1] >= two[1]
+}
+
+function run (file) {
+  const data = fs.readFileSync(file, 'utf8').split('\n').filter(Boolean)
+  const pairs = data.map(pair => {
+    const [ one, two ] = pair.split(',')
+    return {
+      one: one.split('-').map(Number),
+      two: two.split('-').map(Number)
+    }
+  })
+  return pairs.reduce((total, { one, two }) => {
+    return total + (contains(one, two) || contains(two, one) ? 1 : 0)
+  }, 0)
+}
+
+assert.equal(contains([ 6, 6 ], [ 4, 6 ]), false)
+assert.equal(contains([ 4, 6 ], [ 6, 6 ]), true)
+assert.equal(contains([ 2, 8 ], [ 3, 7 ]), true)
+assert.equal(contains([ 3, 7 ], [ 2, 8 ]), false)
+assert.equal(contains([ 5, 7 ], [ 7, 9 ]), false)
+assert.equal(contains([ 7, 9 ], [ 5, 7 ]), false)
+
+//assert.equal(run('./test-data.txt'), 2)
+console.log('total score', run('./data.txt'))
---
+++ 2022/04/part2.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const assert = require('assert')
+
+function disjunct (one, two) {
+  return one[1] < two[0] || two[1] < one[0]
+}
+
+function overlaps (one, two) {
+  return !disjunct(one, two)
+}
+
+function run (file) {
+  const data = fs.readFileSync(file, 'utf8').split('\n').filter(Boolean)
+  const pairs = data.map(pair => {
+    const [ one, two ] = pair.split(',')
+    return {
+      one: one.split('-').map(Number),
+      two: two.split('-').map(Number)
+    }
+  })
+  return pairs.reduce((total, pair) => {
+    return total + (overlaps(pair.one, pair. two) ? 1 : 0)
+  }, 0)
+}
+
+assert.equal(overlaps([ 2, 3 ], [ 4, 5 ]), false)
+assert.equal(overlaps([ 2, 4 ], [ 6, 8 ]), false)
+assert.equal(overlaps([ 5, 7 ], [ 7, 9 ]), true)
+assert.equal(overlaps([ 2, 8 ], [ 3, 7 ]), true)
+assert.equal(overlaps([ 4, 6 ], [ 6, 6 ]), true)
+assert.equal(overlaps([ 2, 6 ], [ 4, 8 ]), true)
+
+//assert.equal(run('./test-data.txt'), 4)
+console.log('total score', run('./data.txt'))
