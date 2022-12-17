07de9a8d36935a32ee583ca20c8bc7d7765c1067
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-12-17 22:17:32 +0100

    2022/11

---
+++ 2022/11/part1.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+const assert = require('assert')
+
+function round (monkeys) {
+  const MONKEYS = monkeys.slice()
+  for (let i = 0; i < MONKEYS.length; ++i) {
+    const m = MONKEYS[i]
+    //console.log('Monkey', i)
+    let { items, op, div, onTrue, onFalse } = m
+    if (items.length === 0) {
+      //console.log('  Monkey has no items, turn ended')
+      continue
+    }
+    for (let j = 0; j < items.length; ++j) {
+      const item = items[j]
+      //console.log('  Monkey inspects an item with a worry level of', item)
+      let worry = eval(`const old = ${item};${op}`)
+      //console.log('    Worry level becomes', worry)
+      worry = Math.floor(worry / 3)
+      //console.log(`    Monkey gets bored with item. Worry level is divided by ${div} to ${worry}`)
+      const isTrue = worry % div === 0 ? true : false
+      const toMonkey = isTrue ? onTrue : onFalse
+      //console.log(`    Item with worry level ${worry} is thrown to ${toMonkey}`)
+      MONKEYS[toMonkey].items.push(worry)
+      m.inspected++
+    }
+    m.items = []
+  }
+  return MONKEYS
+}
+
+function run (file) {
+  let monkeys = fs.readFileSync(file, 'utf8').split('\n\n').map(str => {
+    const rows = str.split('\n').map(s => s.trim())
+    const items = rows[1].split(': ')[1].split(',').map(Number)
+    const op = rows[2].split(' = ')[1]
+    const div = Number(rows[3].split(' divisible by ')[1])
+    const onTrue = Number(rows[4].split(' throw to monkey ')[1])
+    const onFalse = Number(rows[5].split(' throw to monkey ')[1])
+    return {
+      items,
+      op,
+      div,
+      onTrue,
+      onFalse,
+      inspected: 0
+    }
+  })
+
+  for (let i = 0; i < 20; ++i) {
+    console.log('ROUND', i + 1)
+    monkeys = round(monkeys)
+    console.log(monkeys)
+  }
+
+  monkeys.sort((lhs, rhs) => {
+    if (lhs.inspected > rhs.inspected) return -1
+    else if (lhs.inspected < rhs.inspected) return 1
+    else if (lhs.inspected === rhs.inspected) return 0
+  })
+
+  return monkeys[0].inspected * monkeys[1].inspected
+}
+
+assert.equal(run('./test-data.txt'), 10605)
+//console.log('result', run('./data.txt'))
---
+++ 2022/11/part2.js
@@ -0,0 +1,93 @@
+const fs = require('fs')
+const assert = require('assert')
+
+function round (monkeys, monkeyCount, maxFactor) {
+  for (let i = 0; i < monkeyCount; ++i) {
+    const m = monkeys[i]
+    let { items, fn, mod, onTrue, onFalse } = m
+    if (items.length) {
+      for (let j = 0; j < items.length; ++j) {
+        const item = items[j]
+        let worry = fn(item)
+        if (worry > maxFactor) {
+          worry = worry % maxFactor
+        }
+        const isTrue = (!(worry % mod) ? true : false)
+        const toMonkey = isTrue ? onTrue : onFalse
+        monkeys[toMonkey].items.push(worry)
+        m.inspected++
+      }
+      m.items = []
+    }
+  }
+}
+
+function run (file) {
+  let monkeys = fs.readFileSync(file, 'utf8').split('\n\n').map(str => {
+    const rows = str.split('\n').map(s => s.trim())
+    const items = rows[1].split(': ')[1].split(',').map(Number)
+    const op = rows[2].split(' = ')[1]
+    const operator = op.split(' ')[1]
+    const operands = op.split(/ [+*] /)
+    let lastOperand = operands[1]
+    const mod = Number(rows[3].split(' divisible by ')[1])
+    let fn = null
+    if (operator === '+') {
+      if (lastOperand === 'old') {
+        fn = (old) => old + old
+      } else if (!isNaN(Number(lastOperand))) {
+        fn = (old) => old + Number(lastOperand)
+      } else {
+        assert(false, 'should not happen 1')
+      }
+    } else if (operator === '*') {
+      if (lastOperand === 'old') {
+        fn = (old) => old * old
+      } else if (!isNaN(Number(lastOperand))) {
+        fn = (old) => old * Number(lastOperand)
+      } else {
+        assert(false, 'should not happen 1')
+      }
+    } else {
+      assert(false, 'should not happen 2')
+    }
+    const onTrue = Number(rows[4].split(' throw to monkey ')[1])
+    const onFalse = Number(rows[5].split(' throw to monkey ')[1])
+    return {
+      items,
+      fn,
+      mod,
+      onTrue,
+      onFalse,
+      inspected: 0
+    }
+  })
+
+  const maxFactor = monkeys.reduce((tot, m) => {
+    return tot * m.mod
+  }, 1)
+
+  const monkeyCount = monkeys.length
+  const checkRounds = [1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+
+  for (let r = 1; r <= 10000; ++r) {
+    round(monkeys, monkeyCount, maxFactor)
+    if (checkRounds.includes(r)) {
+      console.log('ROUND', r)
+      monkeys.forEach((m, i) => {
+        console.log(' Monkey', i, 'inspected items', m.inspected, 'times')
+      })
+    }
+  }
+
+  monkeys.sort((lhs, rhs) => {
+    if (lhs.inspected > rhs.inspected) return -1
+    else if (lhs.inspected < rhs.inspected) return 1
+    else if (lhs.inspected === rhs.inspected) return 0
+  })
+
+  return monkeys[0].inspected * monkeys[1].inspected
+}
+
+//assert.equal(run('./test-data.txt'), 2713310158)
+console.log('result', run('./data.txt'))
